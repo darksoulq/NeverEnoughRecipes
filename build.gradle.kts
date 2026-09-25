d91dbf51c9e17cf39956ae46565081a5c5e81095
darksoulq <amaanwani1a@gmail.com>
2026-09-25 10:52:13 +0530

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 
 group = "io.github.darksoulq"
 val mcVersion = stonecutter.current.project
-version = "1.6.0-mc.${mcVersion}"
+version = "1.6.1-mc.${mcVersion}"
 
 val yamlParser = Yaml()
 
